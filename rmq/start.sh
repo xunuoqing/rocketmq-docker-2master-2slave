5cd11cffbc5c2db9a898146e932e7da3b9415908
liuquan.06 <liuquan.06@bytedance.com>
2021-04-27 18:22:04 +0800

    init

--- rmq/start.sh
+++ rmq/start.sh
@@ -2,19 +2,48 @@
 
 # 创建目录
 mkdir -p ./rmqs/logs
+mkdir -p ./rmqs/logs
 mkdir -p ./rmqs/store
-mkdir -p ./rmq/logs
-mkdir -p ./rmq/store
+
+mkdir -p ./rmqs2/logs
+mkdir -p ./rmqs2/store
+
+
+mkdir -p ./rmq1/logs
+mkdir -p ./rmq1/store
+
+mkdir -p ./rmq1-slave/logs
+mkdir -p ./rmq1-slave/store
+
+mkdir -p ./rmq2/logs
+mkdir -p ./rmq2/store
+
+mkdir -p ./rmq2-slave/logs
+mkdir -p ./rmq2-slave/store
 
 # 设置目录权限
 chmod -R 777 ./rmqs/logs
 chmod -R 777 ./rmqs/store
-chmod -R 777 ./rmq/logs
-chmod -R 777 ./rmq/store
+
+chmod -R 777 ./rmqs2/logs
+chmod -R 777 ./rmqs2/store
+
+
+chmod -R 777 ./rmq1/logs
+chmod -R 777 ./rmq1/store
+
+chmod -R 777 ./rmq1-slave/logs
+chmod -R 777 ./rmq1-slave/store
+
+chmod -R 777 ./rmq2/logs
+chmod -R 777 ./rmq2/store
+
+chmod -R 777 ./rmq2-slave/logs
+chmod -R 777 ./rmq2-slave/store
 
 # 下载并启动容器，且为 后台 自动启动
-docker-compose up -d
-
+# docker-compose up -d
+docker-compose up
 
 # 显示 rocketmq 容器
-docker ps |grep rocketmq
+docker ps | grep rocketmq
